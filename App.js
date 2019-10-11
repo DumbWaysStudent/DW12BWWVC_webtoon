d3fdef7546585971683878da1ccb04f99adcfc85
zaulsr <zaulsr14@gmail.com>
2019-10-11 13:44:27 +0700

    add edit my webtoon screen

---
+++ App.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+import { Icon } from 'native-base';
+import { Share } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Login from './Pages/Login';
+import ForYou from './Pages/ForYou';
+import DetailScreen from './Pages/DetailScreen';
+import DetailEps from './Pages/DetailEps';
+import FavScreen from './Pages/FavScreen';
+import Profile from './Pages/Profile';
+import EditProfile from './Pages/EditProfile';
+import MyCreation from './Pages/MyCreation';
+import CreateWebtoon from './Pages/CreateWebtoon';
+import CreateEpisode from './Pages/CreateEpisode';
+import EditWebtoon from './Pages/EditWebtoon';
+
+const onShare = async () => {
+  try {
+    const result = await Share.share({
+      message:
+        'React Native | A framework for building native apps using React',
+    });
+
+    if (result.action === Share.sharedAction) {
+      if (result.activityType) {
+        // shared with activity type of result.activityType
+      } else {
+        // shared
+      }
+    } else if (result.action === Share.dismissedAction) {
+      // dismissed
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+const signedOut = createStackNavigator(
+  {
+    Login: {
+      screen: Login,
+      title: 'Login',
+      navigationOptions: { header: null }
+    }
+  }, {
+  initialRouteName: 'Login'
+}
+);
+
+const signedIn = createStackNavigator(
+  {
+    ForYou: {
+      screen: ForYou,
+      title: 'ForYou',
+      navigationOptions: { header: null }
+    },
+    DetailScreen: {
+      screen: DetailScreen,
+      title: 'DetailScreen',
+      navigationOptions: () => ({
+        title: "Detail Webtoon",
+        headerStyle: {
+          backgroundColor: '#38D40A',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerRight: (
+          <Icon name="share-alt" style={{ color: 'white', marginRight: 15, }} onPress={() => onShare()} />
+        ),
+      })
+    },
+
+    FavScreen: {
+      screen: FavScreen,
+      title: 'FavScreen',
+      navigationOptions: { header: null }
+    },
+
+    Profile: {
+      screen: Profile,
+      title: 'Profile'
+    },
+
+    MyCreation: {
+      screen: MyCreation,
+      title: 'MyCreation'
+    },
+
+    EditProfile: {
+      screen: EditProfile,
+      title: 'EditProfile'
+    },
+
+    DetailEps: {
+      screen: DetailEps,
+      title: 'DetailEps'
+    },
+
+    CreateWebtoon: {
+      screen: CreateWebtoon,
+      title: 'CreateWebtoon'
+    },
+
+    CreateEpisode: {
+      screen: CreateEpisode,
+      title: 'CreateEpisode'
+    },
+
+    EditWebtoon: {
+      screen: EditWebtoon,
+      title: 'EditWebtoon'
+    }
+
+  },
+  {
+    initialRouteName: 'ForYou'
+  }
+);
+
+const Switch = createSwitchNavigator({
+  signedIn: signedIn,
+  signedOut: signedOut
+}, {
+  initialRouteName: 'signedOut'
+});
+
+
+export default createAppContainer(Switch);
---
+++ Pages/MyCreation.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native';
+import { Container, Header, Content, Footer, FooterTab, Icon, Text, Item, Input, View, Row, Button, Fab } from 'native-base';
+
+export default class MyCreation extends Component {
+
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: 'My Webtoon',
+            headerStyle: {
+                backgroundColor: '#38D40A'
+            },
+            headerTitleStyle: {
+                fontWeight: 'bold'
+            },
+            headerTintColor: '#fff',
+        };
+    };
+
+    constructor() {
+        super();
+        this.state = {
+            active: false,
+            banners: [{
+                title: 'Pasutri Gaje',
+                image: 'https://4.bp.blogspot.com/-7RzHQQanlqY/XE7r94lzUCI/AAAAAAAACqI/keHhTWrJ1441h7vHWIL_FNf912DnsRMbQCLcBGAs/s1600/Pasutri%2BGaje%2BSeason%2B2%2BAnissa%2BNisfihani%2BWebtoon%2BIndonesia.JPG'
+            }, {
+                title: 'Young Mom',
+                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmeLcEii64s4ZsDUz_Le5B504ibpjOQLvZlfMozb02YuwVhA6s3Q'
+            }, {
+                title: 'Born From Deadth',
+                image: 'https://vignette.wikia.nocookie.net/webtoon/images/b/bd/SAMPUL_BFD.jpg/revision/latest?cb=20180715072050&path-prefix=id'
+            }, {
+                title: 'Terlalu Tampan',
+                image: 'https://damniloveindonesia.com/image/catalog/explore_indonesia/Artikel/Terlalu_Tampan_1.jpg'
+            }]
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+
+                <Content>
+
+                    <FlatList
+                        data={this.state.banners}
+                        renderItem={({ item }) => (
+
+                            <View style={{ marginHorizontal: 20, marginTop: 15 }}>
+                                <Row>
+
+                                    <TouchableOpacity onPress={() => this.props.navigation.navigate('EditWebtoon', { title: item.title })} >
+                                        <Image style={styles.imglist} source={{ uri: item.image }} />
+                                    </TouchableOpacity>
+
+                                    <View style={{ margin: 15 }}>
+                                        <Text style={{ fontWeight: 'bold', fontSize: 20 }}> {item.title} </Text>
+                                        <Text style={{ marginTop: 10 }}> 90 Fav+ </Text>
+                                    </View>
+                                </Row>
+                            </View>
+
+                        )}
+                        keyExtractor={item => item.id}
+                    />
+                </Content>
+
+                <View>
+                    <Fab
+                        active={this.state.active}
+                        containerStyle={{}}
+                        style={{ backgroundColor: '#38D40A' }}
+                        position="bottomRight"
+                        onPress={() => this.props.navigation.navigate('CreateWebtoon')}>
+                        <Icon name="ios-create" />
+                    </Fab>
+                </View>
+
+            </Container >
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+    imglist: {
+        width: 120,
+        height: 120,
+        borderColor: 'black',
+        borderWidth: 2
+    }
+})
